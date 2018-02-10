64cdec289f68869a7b5dd3684fffd6cb7ffa7a7b
81623405 <81623405@qq.com>
2018-02-11 01:07:52 +0800

    jinja2template

---
+++ jinja2template/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+from flask import Flask, render_template
+
+app=Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD']=True
+#with this you don't have to restart the server every time you change app.py or templates, and only have to refresh the page
+
+#add filter
+def hide_email(email):
+	parts=email.split('@')
+	parts[0]='***'
+	return '@'.join(parts)
+
+app.add_template_filter(hide_email)
+
+@app.route('/')
+def index():
+	teacher={
+		'name': 'Aiden',
+		'email': 'luojing@simplecloud.cn'
+	}
+	course={
+	'name': 'Python Basic',
+	'teacher': teacher,
+	'user_count': 5348,
+	'price': 199,
+	'lab': None,
+	'is_private': False,
+	'is_member_course': True,
+	'tags': ['Python','Linux','bigdata']
+	}
+	return render_template('index.html',course=course)
