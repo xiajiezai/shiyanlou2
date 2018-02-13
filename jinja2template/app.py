19d18715e683b276cde431e9c054e5bd64ab2913
xiajiezai <xiajiezai@gmail.com>
2018-02-13 18:21:59 +0800

    Update app.py

--- jinja2template/app.py
+++ jinja2template/app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 app=Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD']=True
 #with this you don't have to restart the server every time you change app.py or templates, and only have to refresh the page
+#in shell type the following to run: FLASK_DEBUG = 1 FLASK_APP=app.py flask run port 5000. port 5000 is optional
 
 #add filter
 def hide_email(email):
