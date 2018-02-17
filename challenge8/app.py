5dc4416b7315030bff7ff511cad3a35262a70af4
81623405 <81623405@qq.com>
2018-02-17 23:17:59 +0800

    add tags

---
+++ challenge8/app.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+app.config.update(dict(
+    SQLALCHEMY_DATABASE_URI='mysql://root@localhost/shiyanlou'
+    ))
+
+db=SQLAlchemy(app)
+mongo=MongoClient('127.0.0.1',27017).shiyanlou
+#.files after shiyanlou can be ommited
+
+class File(db.Model):
+    __tablename__='files'
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(80),unique=True)
+    create_time=db.Column(db.DateTime)
+    category_id=db.Column(db.Integer,db.ForeignKey('categories.id'))
+    #a field in one table that refers to the primary key in another table
+    category=db.relationship('Category',uselist=False)
+    #search for category by file
+    content=db.Column(db.Text)
+
+    def __init__(self,title,create_time,category,content):
+        self.title=title
+        self.create_time=create_time
+        self.category=category
+        self.content=content
+
+    def add_tag(self,tag_name):
+        file_item=mongo.files.find_one({'file_id':self.id})
+        #by find_one, you automatically created files for mongo    
+        if file_item:
+            tags=file_item['tags']
+            if tag_name not in tags:
+                tags.append(tag_name)
+            mongo.files.update_one({'file_id':self.id}, {'$set':{'tags':tags}})
+        else:
+            tags=[tag_name]
+            mongo.files.insert_one({'file_id':self.id, 'tags':tags})
+        return tags
+
+    def remove_tag(self,tag_name):
+        file_item=mongo.files.find_one({'file_id':self.id})
+        if file_item:
+            tags=file_item['tags']
+            try:
+                tags.remove(tag_name)
+                new_tags=tags
+            except ValueError:
+                return tags
+            mongo.files.update_one({'file_id':self.id}, {'$set':{'tags':new_tags}})
+            return new_tags
+        else:
+            return []
+
+    @property
+    def tags(self):
+        file_item=mongo.files.find_one({'file_id':self.id})
+        if file_item:
+            print(file_item)
+            return file_item['tags']
+        else:
+            return []    
+
+class Category(db.Model):
+    __tablename__='categories'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(80))
+    files=db.relationship('File')
+
+    def __init__(self,name):
+        self.name=name
+
+    
+
+
+
+def insert_data():
+    db.create_all()
+    java=Category('Java')
+    python=Category('Python')
+    file1=File('Hello Java',datetime.utcnow(),java,'File Content-Java is cool!')
+    file2=File('Hello Python',datetime.utcnow(),python,'Python Content-Java is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
+    file1.add_tag('tech')
+    file1.add_tag('java')
+    file1.add_tag('linux')
+    file2.add_tag('tech')
+    file2.add_tag('python')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', files = File.query.all())
+
+@app.route('/files/<int:file_id>')
+def file(file_id):
+    file_item = File.query.get_or_404(file_id)
+    return render_template('file.html', file_item=file_item)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+if __name__== '__main__':
+    app.run()
